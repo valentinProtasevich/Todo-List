ad96c660554dd5c3f7a484b6f07c2374c7699214
Valentin <veksel_or_vels@mail.ru>
2021-08-18 21:25:34 +0300

    added some functions

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
 // Форма
 // Список задач
-const tasks = [
-  // {
-  //   _id: '5d2ca9e2e03d40b326596aa7',
-  //   completed: true,
-  //   body:
-  //     'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-  //   title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  // },
-  // {
-  //   _id: '5d2ca9e29c8a94095c1288e0',
-  //   completed: false,
-  //   body:
-  //     'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-  //   title:
-  //     'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  // },
-  // {
-  //   _id: '5d2ca9e2e03d40b3232496aa7',
-  //   completed: true,
-  //   body:
-  //     'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-  //   title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  // },
-  // {
-  //   _id: '5d2ca9e29c8a94095564788e0',
-  //   completed: false,
-  //   body:
-  //     'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-  //   title:
-  //     'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  // },
-];
+const tasks = [];
 
 (function(arrOfTasks) {
   //Переводим массив задач в объект объектов
@@ -91,9 +60,14 @@ const tasks = [
     article.textContent = body;
     article.classList.add('mt-2', 'w-100');
 
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = 'Completed';
+    completeBtn.classList.add('btn', 'btn-success', 'ml-auto', 'complete-btn');
+
     li.appendChild(span);
     li.appendChild(deleteBtn);
     li.appendChild(article);
+    li.appendChild(completeBtn);
     
     return li;
   };
@@ -113,6 +87,7 @@ const tasks = [
     const listItem = listItemTemplate(task);
     listContainer.insertAdjacentElement('afterbegin', listItem);
     form.reset();
+    haveTasks()
   };
 
   function createNewTask(title, body) {
@@ -148,5 +123,19 @@ const tasks = [
       const confirmed = deleteTask(id);
       deleteTaskFromHtml(confirmed ,parent);
     };
+    haveTasks()
   };
+
+  //Проверка наличия задачи
+  function haveTasks() {
+    if (document.querySelector('li') === null) {
+      form.insertAdjacentHTML('afterend', '<p class="btn btn-danger mt-2" id = "noTasks">No tasks</p>');
+    } else {
+      document.querySelector('#noTasks').remove();
+    };
+  };
+  haveTasks();
+
+
 })(tasks);
+
